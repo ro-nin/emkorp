ed15302357a4b1691bae2057626217cf3ed53433
ro-nin <ema.cio1793@gmail.com>
2022-06-29 15:04:47 +0200

    update sharp

--- components/ProjectPreview.js
+++ components/ProjectPreview.js
@@ -7,15 +7,14 @@ import ProjectButtons from './ProjectButtons';
 
 export default function ProjectPreview({ data, selected, onPress }) {
     const sharedTranslator = useTranslations('shared');
+    //todo move to css classe
     const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-
     const baseStyle = ' border border-black transition-all duration-500 ease-in-out relative  flex flex-col lg:inline-flex mt-12 lg:m-0 hover:cursor-pointer '
     const size = selected ? ' flex-grow6 min-h-[200px] lg:flex-grow8 lg:max-h-[100%] lg:max-w-[70%] ' : 'flex-grow1 min-h-[60px]  lg:max-h-[90%] '
     const grayscale = selected ? ' grayscale-0 hover:grayscale-0' : ' grayscale hover:grayscale-0'
     const borderStyle = selected ? ' lg:shadowed' : '  '
     const fullStyle = baseStyle + size + grayscale + borderStyle;
     let imageUrl = '/' + (data.previewImage??data.images[0]); 
-    console.log(data)
     return (
 
         <>
